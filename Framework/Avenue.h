055234d94e0f510c7e2eb5a155cb929dd28884dd
Aleksandar Vacic <av@codeaplus.com>
2019-02-17 12:46:54 +0100

    Added framework project, to prepare for Carthage support

---
+++ Framework/Avenue.h
@@ -0,0 +1,19 @@
+//
+//  Avenue.h
+//  Avenue
+//
+//  Created by Aleksandar Vacić on 2/17/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Avenue.
+FOUNDATION_EXPORT double AvenueVersionNumber;
+
+//! Project version string for Avenue.
+FOUNDATION_EXPORT const unsigned char AvenueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Avenue/PublicHeader.h>
+
+
