47b8aa4bdac251b11c117f627ed14615889d16de
Aleksandar Vacic <av@codeaplus.com>
2019-02-18 11:36:01 +0100

    A bit more comments and explanation for the possible errors.

--- Avenue/NetworkError.swift
+++ Avenue/NetworkError.swift
@@ -11,13 +11,18 @@ import Foundation
 ///	Various error this library is specifically handling.
 ///	If you extend the capabilities of the this micro-library, you may add as many cases as you need here
 public enum NetworkError: Error {
+	//	Returned URLResponse is not HTTPURLResponse
 	case invalidResponse
 
+	//	Returned HTTPURLResponse has no body (see `NetworkHTTPMethod`)
 	case noData
 
+	//	NetworkOperation is cancelled before it finished
 	case cancelled
 
+	//	URLError returned by URLSession framework
 	case urlError(URLError)
 
+	//	Some non-URLError returned by URLSession framework
 	case other(Swift.Error)
 }
