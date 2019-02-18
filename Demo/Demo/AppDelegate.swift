5cdad1f3aff450bf99847753dd42c99249462e3d
Aleksandar Vacic <av@codeaplus.com>
2019-02-18 12:29:47 +0100

    Added example how to setup default ServerTrustPolicy.
    This is usually not done in AppDelegate but in some configuration file, per target.

--- Demo/Demo/AppDelegate.swift
+++ Demo/Demo/AppDelegate.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import Avenue
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
+
+	func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		//	Example: disable HTTPS server-trust checks for development
+		ServerTrustPolicy.defaultPolicy = .disableEvaluation
+
+		return true
+	}
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		return true
