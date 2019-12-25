d0f136f36f4c683f055668eedd53877129b528a1
Aleksandar Vacić <av@radianttap.com>
2019-12-25 12:48:00 +0100

    Added Alley, as straight-up copy.
    Avenue v4 will build on top of it.

---
+++ Alley/NetworkError.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkError.swift
+//  Alley
+//
+//  Copyright © 2019 Radiant Tap
+//  MIT License · http://choosealicense.com/licenses/mit/
+//
+
+import Foundation
+
+/**
+Declaration of errors that Alley can throw/return.
+
+Since this is all about networking, it should pass-through any URLErrors that happen but also add its own
+*/
+public enum NetworkError: Error {
+	///	When network conditions are so bad that after `maxRetries` the request did not succeed.
+	case inaccessible
+
+	///	`URLSession` errors are passed-through, handle as appropriate.
+	case urlError(URLError)
+
+	///	URLSession returned an `Error` object which is not `URLError`
+	case generalError(Swift.Error)
+
+	///	When no `URLResponse` is returned but also no `URLError` or any other `Error` instance.
+	case noResponse
+
+	///	When `URLResponse` is not `HTTPURLResponse`.
+	case invalidResponseType(URLResponse)
+
+	///	Status code is in `200...299` range, but response body is empty. This can be both valid and invalid, depending on HTTP method and/or specific behavior of the service being called.
+	case noResponseData(HTTPURLResponse)
+
+	///	Status code is `400` or higher thus return the entire `HTTPURLResponse` and `Data` so caller can figure out what happened.
+	case endpointError(HTTPURLResponse, Data?)
+}
---
+++ Alley/URLSession-extensions.swift
@@ -0,0 +1,146 @@
+//
+//  URLSession-extensions.swift
+//  Alley
+//
+//  Copyright © 2019 Radiant Tap
+//  MIT License · http://choosealicense.com/licenses/mit/
+//
+
+import Foundation
+
+public extension URLSession {
+	///	Default number of retries to attempt on each `URLRequest` instance. To customize, supply desired value to `perform()`
+	static var maximumNumberOfRetries: Int = 10
+
+	///	Output types
+	typealias DataResult = Result<Data, NetworkError>
+	typealias Callback = (DataResult) -> Void
+
+	/// Executes given URLRequest instance, possibly retrying the said number of times. Through `callback` returns either `Data` from the response or `NetworkError` instance.
+	/// If any authentication needs to be done, it's handled internally by this methods and its derivatives.
+	/// - Parameters:
+	///   - urlRequest: URLRequest instance to execute.
+	///   - maxRetries: Number of automatic retries (default is 10).
+	///   - allowEmptyData: Should empty response `Data` be treated as failure (this is default) even if no other errors are returned by URLSession. Default is `false`.
+	///   - callback: Closure to return the result of the request's execution.
+	func perform(_ urlRequest: URLRequest,
+				 maxRetries: Int = URLSession.maximumNumberOfRetries,
+				 allowEmptyData: Bool = false,
+				 callback: @escaping Callback)
+	{
+		if maxRetries <= 0 {
+			fatalError("maxRetries must be 1 or larger.")
+		}
+
+		let networkRequest = NetworkRequest(urlRequest, 0, maxRetries, allowEmptyData, callback)
+		authenticate(networkRequest)
+	}
+}
+
+private extension URLSession {
+	///	Helper type which groups `URLRequest` (input), `Callback` from the caller (output)
+	///	along with helpful processing properties, like number of retries.
+	typealias NetworkRequest = (
+		urlRequest: URLRequest,
+		currentRetries: Int,
+		maxRetries: Int,
+		allowEmptyData: Bool,
+		callback: Callback
+	)
+
+	///	Extra-step where `URLRequest`'s authorization should be handled, before actually performing the URLRequest in `execute()`
+	func authenticate(_ networkRequest: NetworkRequest) {
+		let currentRetries = networkRequest.currentRetries
+		let max = networkRequest.maxRetries
+		let callback = networkRequest.callback
+
+		if currentRetries >= max {
+			//	Too many unsuccessful attemps
+			callback( .failure( .inaccessible ) )
+		}
+
+		//	NOTE: this is the place to handle OAuth2
+		//	or some other form of URLRequest‘s authorization
+
+		//	now execute the request
+		execute(networkRequest)
+	}
+
+	///	Creates the instance of `URLSessionDataTask`, performs it then lightly processes the response before calling `validate`.
+	func execute(_ networkRequest: NetworkRequest) {
+		let urlRequest = networkRequest.urlRequest
+
+		let task = dataTask(with: urlRequest) {
+			[unowned self] data, urlResponse, error in
+
+			let dataResult = self.process(data, urlResponse, error, for: networkRequest)
+			self.validate(dataResult, for: networkRequest)
+		}
+
+		task.resume()
+	}
+
+	///	Process results of `URLSessionDataTask` and converts it into `DataResult` instance
+	func process(_ data: Data?, _ urlResponse: URLResponse?, _ error: Error?, for networkRequest: NetworkRequest) -> DataResult {
+		let allowEmptyData = networkRequest.allowEmptyData
+
+		if let urlError = error as? URLError {
+			return .failure( NetworkError.urlError(urlError) )
+
+		} else if let otherError = error {
+			return .failure( NetworkError.generalError(otherError) )
+		}
+
+		guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
+			if let urlResponse = urlResponse {
+				return .failure( NetworkError.invalidResponseType(urlResponse) )
+			} else {
+				return .failure( NetworkError.noResponse )
+			}
+		}
+
+		if httpURLResponse.statusCode >= 400 {
+			return .failure( NetworkError.endpointError(httpURLResponse, data) )
+		}
+
+        guard let data = data, !data.isEmpty else {
+			if allowEmptyData {
+				return .success(Data())
+			}
+
+			return .failure( NetworkError.noResponseData(httpURLResponse) )
+		}
+
+		return .success(data)
+	}
+
+	///	Checks the result of URLSessionDataTask and if there were errors, should the URLRequest be retried.
+	func validate(_ result: DataResult, for networkRequest: NetworkRequest) {
+		let callback = networkRequest.callback
+
+		switch result {
+		case .success:
+			break
+
+		case .failure(let networkError):
+			switch networkError {
+			case .inaccessible:
+				//	too many failed network calls
+				break
+
+			default:
+				if networkError.shouldRetry {
+					//	update retries count and
+					var newRequest = networkRequest
+					newRequest.currentRetries += 1
+					//	try again, going through authentication again
+					//	(since it's quite possible that Auth token or whatever has expired)
+					self.authenticate(newRequest)
+					return
+				}
+			}
+		}
+
+		callback(result)
+	}
+}
